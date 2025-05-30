2838795ff32cd5bf3d7e9e5c5660be45b342d91e
Jorge Villarreal <jv7484031@gmail.com>
2025-05-29 19:06:08 -0600

    Nuevos projetos de csharp

---
+++ computadora/computadora/ComponentesInternos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computadora
+{
+    public class ComponentesInternos : FuentePoder
+    {
+        public ComponentesInternos(bool encendido) : base(encendido)
+        {
+        }
+        public override void CambiarEstado(bool estado)
+        {
+            Encendido = estado;
+        }
+        public void VerificarInternos()
+        {
+            if (Encendido == true)
+            {
+                Console.WriteLine("Todos los componentes internos están funcionando correctamente.");
+            }
+            else
+            {
+                Console.WriteLine("El equipo está apagado. No se puede verificar componentes.");
+            }
+        }
+    }
+}
+// Verifica si los componentes internos están disponibles dependiendo del estado de encendido.
---
+++ computadora/computadora/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            {
+                Console.WriteLine($"El estado de la computadora actualmente es ¿[Encendido] o [apagado]?");
+                string respuesta = Console.ReadLine()?.Trim().ToLower();    
+
+                bool estaEncendida = respuesta == "encendido";
+
+                ComponentesInternos misInternos = new ComponentesInternos(estaEncendida);
+                ElementosExternos misExternos = new ElementosExternos(estaEncendida);
+                Console.WriteLine(new string('=', 60));
+                MostrarEstado(misInternos, misExternos);
+                Console.WriteLine(new string('=', 60));
+
+                {
+                Console.WriteLine($"¿El usuario esta sentado de forma correcta? [Si] o [No]");
+                string posicion = Console.ReadLine()?.Trim().ToLower();
+
+                if(posicion == "si")
+                {
+                    Console.WriteLine("Perfecto, ya puedes usar tu equipo perfectamente");
+                    MostrarEstado(misInternos, misExternos);
+                } else
+                {
+                    Console.WriteLine("Debes sentarte de forma correcta para usar el equipo");
+                    Console.WriteLine("");
+                    Console.WriteLine("¿Ya estas sentado de forma correcta? [Si] o [No]");
+                    string respuestafinal = Console.ReadLine()?.Trim().ToLower();
+                    switch(respuestafinal) {
+                        case "si":
+                            Console.WriteLine("Perfecto, ya puedes usar tu equipo perfectamente");
+                            MostrarEstado(misInternos, misExternos);
+                            break;
+                        case "no":
+                            Console.WriteLine("Lo siento pero no podras usar el equipo hoy");
+                            break;
+                    }
+                }
+            }
+        }
+        static void MostrarEstado(ComponentesInternos internos, ElementosExternos externos)
+        {
+            externos.VerificarExternos();
+            internos.VerificarInternos();
+        }
+
+    }
+}
